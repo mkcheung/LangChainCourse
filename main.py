939cca3bc0149235722a2b3a34abeedba40488cc
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-04-28 22:23:49 -0700

    established connection with gemma 3 via ollama

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import PromptTemplate
 load_dotenv()
 
 def main():
-    print("Hello from langchain-course!")
+    # print("Hello from langchain-course!")
+
+    summary_template = """ given the information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+
+    # llm = ChatOpenAI(temperature=0, model="gpt-5")
+    llm = ChatOllama(temperature=0, model="gemma3:270m")
+    chain = summary_prompt_template | llm
+
+    information = "Elon Musk is a billionaire entrepreneur known for Tesla and SpaceX."
+    response = chain.invoke(input={"information": information})
+    print(response.content)
 
 if __name__ == "__main__":
     main()
