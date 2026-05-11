ad4a044cdc2333ca5e1cd50604ce0a977b289e55
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-05-10 23:14:34 -0700

    illustrate use of tavily search

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch
+from tavily import TavilyClient
+
+tavily = TavilyClient()
 
 def search(query: str) -> str:
     """
@@ -16,15 +20,17 @@ def search(query: str) -> str:
         The search results
     """
     print(f"Searching for {query}")
-    return "Tokyo weather is sunny"
+    return tavily.search(query=query)
+
 
 # llm = ChatOpenAI(model='gpt-5')
 llm = ChatOllama(model="llama3.2")
 tools = [search]
+# tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 def main():
     print('Hello from langchain-course!')
-    result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokyo?")})
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?")})
     for message in result["messages"]:
         message.pretty_print()
 
