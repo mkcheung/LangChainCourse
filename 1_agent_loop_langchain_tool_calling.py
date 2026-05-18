e332a60f9a1b622cac11f992bb7f3b1ca24e7456
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-05-17 21:27:38 -0700

    allow llm invokement of tools

--- 1_agent_loop_langchain_tool_calling.py
+++ 1_agent_loop_langchain_tool_calling.py
@@ -60,6 +60,34 @@ def run_agent(question:str):
         ),
         HumanMessage(content=question)
     ]
+    for iteration in range(1, MAX_INTERATIONS + 1):
+        print(f"\n--- Iteration {interation} ---")
+        ai_message = llm_with_tools.invoke(messages)
+
+        tool_calls = ai_message.tool_calls
+        if not tool_calls:
+            print(f"\nFinal Answer: {ai_message.content}")
+            return ai_message.content
+        tool_call = tool_calls[0]
+        tool_name = tool_call.get("name")
+        tool_args = tool_call.get("args", {})
+        tool_call_id = tool_call.get("id")
+
+        print(f".  [Tool Selected] {tool_name} with args: {tool_args}")
+
+        tool_to_use = tools_dict.get(tool_name)
+        if tool_to_use is None:
+            raise ValueError(f"Tool '{tool_name}' not found")
+
+        observation = tool_to_use.invoke(tool_args)
+
+        print(f"    [Tool Result] {observation}")
+        messages.append(ai_message)
+        messages.append(
+            ToolMessage(content=str(observation), tool_call_id=tool_call_id)
+        )
+    print("ERROR: Max iterations reached without a final answer")
+    return None
 
 if __name__ == "__main__":
     print("Hello LangChain Agent (.bind_tools)!")
